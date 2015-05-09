f26589619c4b1bb542c7b2847113a0d6950c5197
Shawn Lin <jallen0927@hotmail.com>
2015-05-09 12:58:39 +1200

    config and gitignore file

--- shop/config.php
+++ shop/config.php
@@ -1,25 +1,25 @@
 <?php
-// HTTP
-define('HTTP_SERVER', 'http://localhost/shop/');
 
-// HTTPS
-define('HTTPS_SERVER', 'http://localhost/shop/');
+// HTTP
+define('HTTP_SERVER', 'http://' . $_SERVER['HTTP_HOST'] . rtrim(dirname($_SERVER['SCRIPT_NAME']), '/.\\') . '/');
+define('HTTPS_SERVER', 'http://' . $_SERVER['HTTP_HOST'] . rtrim(dirname($_SERVER['SCRIPT_NAME']), '/.\\') . '/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/xampp/htdocs/shop/catalog/');
-define('DIR_SYSTEM', 'C:/xampp/htdocs/shop/system/');
-define('DIR_LANGUAGE', 'C:/xampp/htdocs/shop/catalog/language/');
-define('DIR_TEMPLATE', 'C:/xampp/htdocs/shop/catalog/view/theme/');
-define('DIR_CONFIG', 'C:/xampp/htdocs/shop/system/config/');
-define('DIR_IMAGE', 'C:/xampp/htdocs/shop/image/');
-define('DIR_CACHE', 'C:/xampp/htdocs/shop/system/cache/');
-define('DIR_DOWNLOAD', 'C:/xampp/htdocs/shop/system/download/');
-define('DIR_UPLOAD', 'C:/xampp/htdocs/shop/system/upload/');
-define('DIR_MODIFICATION', 'C:/xampp/htdocs/shop/system/modification/');
-define('DIR_LOGS', 'C:/xampp/htdocs/shop/system/logs/');
-
+define('DIR_OPENCART', str_replace('\'', '/', realpath(dirname(__FILE__))) . '/');
+define('DIR_APPLICATION', DIR_OPENCART.'catalog/');
+define('DIR_SYSTEM', DIR_OPENCART.'system/');
+define('DIR_LANGUAGE', DIR_OPENCART.'catalog/language/');
+define('DIR_TEMPLATE', DIR_OPENCART.'catalog/view/theme/');
+define('DIR_CONFIG', DIR_OPENCART.'system/config/');
+define('DIR_IMAGE', DIR_OPENCART.'image/');
+define('DIR_CACHE', DIR_OPENCART.'system/cache/');
+define('DIR_DOWNLOAD', DIR_OPENCART.'system/download/');
+define('DIR_UPLOAD', DIR_OPENCART.'system/upload/');
+define('DIR_MODIFICATION', DIR_OPENCART.'system/modification/');
+define('DIR_LOGS', DIR_OPENCART.'system/logs/');
 
 define('LANG', 'catalog');
+
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
