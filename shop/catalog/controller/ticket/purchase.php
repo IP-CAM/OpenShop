d51de747c12a7a11c6d2fb58003eed16ff222bbb
FRAMCAD-Michael <ylu633@aucklanduni.ac.nz>
2015-05-09 13:11:46 +1200

    Ticket purchase controller -finished

--- shop/catalog/controller/ticket/purchase.php
+++ shop/catalog/controller/ticket/purchase.php
@@ -36,7 +36,7 @@ class ControllerTicketPurchase extends Controller {
 			$data['button_checkout'] = $this->language->get('button_checkout');
 
 
-			$data['action'] = $this->url->link('ticket/purchase/edit');
+			$data['action'] = $this->url->link('ticket/purchase/checkout');
 
 			$data['products'] = array();
         $event=array('event_id'=>'50');
@@ -72,119 +72,17 @@ class ControllerTicketPurchase extends Controller {
 			}
 	}
 
-	public function add() {
-		$this->load->language('checkout/cart');
+    //if a user is logged, redirect to checkout page
+    //otherwise, redirect to register page
+    public function checkout($data){
+        $this->session->data['purcase_item']=$data;
+        if (!$this->customer->isLogged()) {
+            $this->session->data['redirect'] = $this->url->link('account/account', '', 'SSL');
 
-		$json = array();
-
-		if (isset($this->request->post['product_id'])) {
-			$product_id = (int)$this->request->post['product_id'];
-		} else {
-			$product_id = 0;
-		}
-
-		$this->load->model('catalog/product');
-
-		$product_info = $this->model_catalog_product->getProduct($product_id);
-
-		if ($product_info) {
-			if (isset($this->request->post['quantity'])) {
-				$quantity = (int)$this->request->post['quantity'];
-			} else {
-				$quantity = $product_info['minimum'] ? $product_info['minimum'] : 1;
-			}
-
-			if (isset($this->request->post['option'])) {
-				$option = array_filter($this->request->post['option']);
-			} else {
-				$option = array();
-			}
-
-			$product_options = $this->model_catalog_product->getProductOptions($this->request->post['product_id']);
-
-			foreach ($product_options as $product_option) {
-				if ($product_option['required'] && empty($option[$product_option['product_option_id']])) {
-					$json['error']['option'][$product_option['product_option_id']] = sprintf($this->language->get('error_required'), $product_option['name']);
-				}
-			}
-
-
-			$recurrings = $this->model_catalog_product->getProfiles($product_info['product_id']);
-
-
-
-			if (!$json) {
-				$this->cart->add($this->request->post['product_id'], $quantity, $option, $recurring_id);
-
-				$json['success'] = sprintf($this->language->get('text_success'), $this->url->link('product/product', 'product_id=' . $this->request->post['product_id']), $product_info['name'], $this->url->link('checkout/cart'));
-
-				unset($this->session->data['shipping_method']);
-				unset($this->session->data['shipping_methods']);
-				unset($this->session->data['payment_method']);
-				unset($this->session->data['payment_methods']);
-
-				// Totals
-				$this->load->model('extension/extension');
-
-				$total_data = array();
-				$total = 0;
-				$taxes = $this->cart->getTaxes();
-
-				// Display prices
-				if (($this->config->get('config_customer_price') && $this->customer->isLogged()) || !$this->config->get('config_customer_price')) {
-					$sort_order = array();
-
-					$results = $this->model_extension_extension->getExtensions('total');
-
-					foreach ($results as $key => $value) {
-						$sort_order[$key] = $this->config->get($value['code'] . '_sort_order');
-					}
-
-					array_multisort($sort_order, SORT_ASC, $results);
-
-					foreach ($results as $result) {
-						if ($this->config->get($result['code'] . '_status')) {
-							$this->load->model('total/' . $result['code']);
-
-							$this->{'model_total_' . $result['code']}->getTotal($total_data, $total, $taxes);
-						}
-					}
-
-					$sort_order = array();
-
-					foreach ($total_data as $key => $value) {
-						$sort_order[$key] = $value['sort_order'];
-					}
-
-					array_multisort($sort_order, SORT_ASC, $total_data);
-				}
-
-				$json['total'] = sprintf($this->language->get('text_items'), $this->cart->countProducts() + (isset($this->session->data['vouchers']) ? count($this->session->data['vouchers']) : 0), $this->currency->format($total));
-			} else {
-				$json['redirect'] = str_replace('&amp;', '&', $this->url->link('product/product', 'product_id=' . $this->request->post['product_id']));
-			}
-		}
-
-		$this->response->addHeader('Content-Type: application/json');
-		$this->response->setOutput(json_encode($json));
-	}
-
-    //validate selected quantity for a ticket
-    public function update($data) {
-        $this->load->language('ticket/purchase');
-        $json=$this->ticket->validateQuantity($data);
-        $this->response->addHeader('Content-Type: application/json');
-        $this->response->setOutput(json_encode($json));
-    }
-
-    public function getTotal($data){
-
-        //not finished yet
-        $json=array();
-
-        if(isset($this->session->data['tickets'])&&!empty($this->session->data['tickets'])){
-
-
+            $this->response->redirect($this->url->link('account/register', '', 'SSL'));
         }
-    }
+        else{
+            $this->response->redirect($this->url->link('ticket/checkout', '', 'SSL'));
+        }
+        }
 }
